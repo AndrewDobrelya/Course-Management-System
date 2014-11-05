62f54c141d93ba9176a5a0dcb0a7b03b2091ee68
Danil Stulbo <danilstulbo@gmail.com>
2014-11-05 17:50:07 +0200

    Init repository with gitignore

---
+++ CourseManagementSystem/CourseManagementSystem/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CourseManagementSystem.Models
+{
+    public partial class Question
+    {
+        public Question()
+        {
+            this.Answer = new HashSet<Answer>();
+        }
+
+        public int QuestionId { get; set; }
+        public int Number { get; set; }
+        [Display(Name = "Текст")]
+        public string Text { get; set; }
+
+        public virtual ICollection<Answer> Answer { get; set; }
+        public virtual Test Test { get; set; }
+    }
+}
---
+++ CourseManagementSystem/CourseManagementSystem/Models/Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CourseManagementSystem.Models
+{
+    public partial class Test
+    {
+        public int Id { get; set; }
+        public Test()
+        {
+            this.Question = new HashSet<Question>();
+        }
+
+        public int LastLectureId { get; set; }
+        [Display(Name = "Номер")]
+        public int Number { get; set; }
+
+        public virtual Lecture Lecture { get; set; }
+        public virtual ICollection<Question> Question { get; set; }
+    }
+}
