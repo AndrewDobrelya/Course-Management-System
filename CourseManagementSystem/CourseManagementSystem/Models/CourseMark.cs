db47447e74333a9cd431929ada6f0115d25915ea
AlexeyDidkovskiy <alexeydidkovskiy@gmail.com>
2014-11-24 14:29:20 +0200

    Created model and view Usr

---
+++ CourseManagementSystem/CourseManagementSystem/Models/CourseMark.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseManagementSystem.Models
+{
+    public class CourseMark
+    {
+        [Key]
+        public int id { get; set; }
+        public ApplicationUser user { get; set; }
+        public Course course { get; set; }
+        public int mark { get; set; }
+    }
+}
